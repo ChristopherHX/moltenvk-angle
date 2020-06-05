6a252cc57fe20faec9002a3f3c203ce7188ae308
elix22 <elix22@gmail.com>
2020-06-05 09:34:20 +0300

    [ios] fix compilation error

--- Source/ThirdParty/Civetweb/src/civetweb.c
+++ Source/ThirdParty/Civetweb/src/civetweb.c
@@ -23,6 +23,7 @@
 
 // Modified by cosmy1, Yao Wei Tjong & Lasse Oorni for Urho3D
 
+#include <stdlib.h>
 
 #if defined(__GNUC__) || defined(__MINGW32__)
 #define GCC_VERSION                                                            \
