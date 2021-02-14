f596a84f0ac63ccb277c853ed4162e3f0cdc4f48
Eli Aloni <elix22@gmail.com>
2021-02-14 20:12:01 +0200

    add support of cpying libraries and assemblies from Urho3D build tree to Urho.Net folder

---
+++ script/copy_urhonet_libs.sh
@@ -0,0 +1,35 @@
+URHONET_HOME_ROOT=$(cat ~/.urhonet_config/urhonethome)
+URHO3D_HOME=$(pwd)
+
+if [ ! -d "$URHONET_HOME_ROOT" ]; then
+    echo  "Urho.Net is not configured , please  run configure.sh (configure.bat on Windows) from the Urho.Net installation folder  "
+    exit -1
+else
+    echo "URHONET_HOME_ROOT=${URHONET_HOME_ROOT}"
+fi
+
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/desktop
+cp -f ${URHO3D_HOME}/DotNet/UrhoDotNet/desktop/UrhoDotNet.dll ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/desktop
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/mobile/ios
+cp -f ${URHO3D_HOME}/DotNet/UrhoDotNet/mobile/ios/UrhoDotNet.dll ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/mobile/ios
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/mobile/android
+cp -f ${URHO3D_HOME}/DotNet/UrhoDotNet/mobile/android/UrhoDotNet.dll  ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/mobile/android
+
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/ios/urho3d/gles
+cp -f ${URHO3D_HOME}/DotNet/libs/iphone/Release-iphoneos/libUrho3D-GLES.a  ${URHONET_HOME_ROOT}/template/libs/ios/urho3d/gles/libUrho3D.a
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/ios/urho3d/metal
+cp -f ${URHO3D_HOME}/DotNet/libs/iphone/Release-iphoneos/libUrho3D-Metal.a ${URHONET_HOME_ROOT}/template/libs/ios/urho3d/metal/libUrho3D.a
+
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/macos
+cp -f ${URHO3D_HOME}/DotNet/libs/macos/Release/libUrho3D.dylib  ${URHONET_HOME_ROOT}/template/libs/macos
+
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/windows
+cp -f ${URHO3D_HOME}/DotNet/libs/windows/release/Urho3D.dll  ${URHONET_HOME_ROOT}/template/libs/windows
+
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/linux
+cp -f ${URHO3D_HOME}/DotNet/libs/linux/release/libUrho3D.so  ${URHONET_HOME_ROOT}/template/libs/linux
+
+cp -f ${URHO3D_HOME}/DotNet/libs/android/release/lib/arm64-v8a/libUrho3D.so     ${URHONET_HOME_ROOT}/template/Android/app/src/main/jniLibs/arm64-v8a
+cp -f ${URHO3D_HOME}/DotNet/libs/android/release/lib/armeabi-v7a/libUrho3D.so     ${URHONET_HOME_ROOT}/template/Android/app/src/main/jniLibs/armeabi-v7a
+cp -f ${URHO3D_HOME}/DotNet/libs/android/release/lib/x86/libUrho3D.so     ${URHONET_HOME_ROOT}/template/Android/app/src/main/jniLibs/x86
+cp -f ${URHO3D_HOME}/DotNet/libs/android/release/lib/x86_64/libUrho3D.so     ${URHONET_HOME_ROOT}/template/Android/app/src/main/jniLibs/x86_64
