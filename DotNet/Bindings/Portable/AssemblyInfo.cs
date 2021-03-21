5edbb045d94e328c91acbaf30aa9606902b15d04
Eli Aloni <elix22@gmail.com>
2021-03-22 00:13:26 +0200

    [dotnet] Urho.Net version 0.0.0.3

--- DotNet/Bindings/Portable/AssemblyInfo.cs
+++ DotNet/Bindings/Portable/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 
-[assembly: AssemblyVersion("0.0.0.2")]
-[assembly: AssemblyFileVersion("0.0.0.2")]
+[assembly: AssemblyVersion("0.0.0.3")]
+[assembly: AssemblyFileVersion("0.0.0.3")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
