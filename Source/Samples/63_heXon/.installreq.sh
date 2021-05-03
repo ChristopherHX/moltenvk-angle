d8ba37e51ba72a50e97862000e6f4f2e347b7217
Eli <elix22@gmail.com>
2021-05-03 12:33:14 +0300

    POC Google admob

--- Source/Samples/63_heXon/.installreq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install \
-    libx11-dev libxrandr-dev libasound2-dev \
-    git make cmake build-essential
--- Source/Samples/66_SparkDemo/Urhox/Urhox.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "SystemUI/SystemUI.h"
-#include "SystemUI/DebugHud.h"
-#include "SystemUI/Console.h"
-#include "Spark/UrhoSpark.h"
-
