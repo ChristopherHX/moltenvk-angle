8046fa703d01c3fcd3bd5f6cbf3ba257493349dc
Eli <elix22@gmail.com>
2022-02-02 17:59:13 +0200

    [dotnet] Assimp , fix Windows link errors

--- script/build_windows_tools.ps1
+++ script/build_windows_tools.ps1
@@ -1,7 +1,3 @@
-cmd /c rmdir build-vs2019-tools /s /q 
 script/cmake_vs2019_tools.bat build-vs2019-tools
-cmake --build  build-vs2019-tools/Source/ThirdParty/Assimp/.  --config Release
 cmake --build  build-vs2019-tools/. --config Release
-Copy-Item   build-vs2019-tools/bin/Assimp.dll  -Destination build-vs2019-tools/bin/tool
-Copy-Item   build-vs2019-tools/bin/draco.dll  -Destination build-vs2019-tools/bin/tool
 
