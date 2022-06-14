16d830a8bb7dcd19d8836f3ecce9843e6a9a6673
Eli Aloni <elix22@gmail.com>
2022-06-14 12:25:49 +0300

    [dotnet] UrhoEventAdapter.cs , remove invalid RefCounted objects from listOfManagedSubscribers

--- DotNet/Bindings/Portable/Runtime/UrhoEventAdapter.cs
+++ DotNet/Bindings/Portable/Runtime/UrhoEventAdapter.cs
@@ -39,7 +39,44 @@ namespace Urho
 						for (int i = 0; i < count; i++)
 						{
 							if (i < listOfManagedSubscribers.Count)
-								listOfManagedSubscribers[i](args);
+							{
+                                if (i >= 0)
+                                {
+									// elix22
+									// If RefCounted object gets deleted/disposed , it doesn't notify all UrhoEventAdapters  that it's subscribed to  .
+									// It is expected from the developer to  unsubscribe any RefCounted(Node , Component , ...) before it's deleted. 
+									// But as we all know that it's not always the case.
+									// If the RefCounted object is part of  the listOfManagedSubscribers then it must be first checked that it's valid.
+									// if it's already deleted then remove it from the listOfManagedSubscribers
+                                    RefCounted refCounted = listOfManagedSubscribers[i].Target as RefCounted;
+                                    if (refCounted != null && refCounted.IsDeleted)
+                                    {
+										// invalid managed subscriber , remove it and continue to the next in the list.
+                                        listOfManagedSubscribers.Remove(listOfManagedSubscribers[i]);
+										// elix22
+										// the size of listOfManagedSubscribers is reduced by 1 , now i points the next managed subscriber , 
+										// Above loop will increase i and the call to the next managed subscriber will be missed.
+										// So i must be adjusted and decreased by 1.
+                                        i--;
+                                        continue;
+                                    }
+
+									// elix22 , valid managed subscriber , call it.
+                                    listOfManagedSubscribers[i](args);
+                                }
+							}
+							else
+							{
+								break;
+							}
+						}
+
+						// elix22 
+						// Might occur in case the listOfManagedSubscribers contained only RefCounted objects that were  all removed 
+						if (listOfManagedSubscribers.Count < 1)
+						{
+							managedSubscribersByObjects.Remove(handle);
+							nativeSubscriptionsForObjects[handle].Unsubscribe();
 						}
 					});
 			}
@@ -52,6 +89,8 @@ namespace Urho
 
 		public void RemoveManagedSubscriber(IntPtr handle, Action<TEventArgs> action)
 		{
+			var target = action.Target;
+
 			List<Action<TEventArgs>> listOfManagedSubscribers;
 			if (managedSubscribersByObjects.TryGetValue(handle, out listOfManagedSubscribers))
 			{
